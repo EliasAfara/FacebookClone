665053974fd0b1a8565023dac42461881d6e58ec
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-02-23 18:13:30 +0200

    🔒 Modified Firebase Environment Variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth'; // for authentication
 import 'firebase/firestore'; // for cloud firestore
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBb9NUVLg8ThRaF6XmLfxiyXS1-Fq6mUk4',
-  authDomain: 'facebook-clone-8f0b2.firebaseapp.com',
-  projectId: 'facebook-clone-8f0b2',
-  storageBucket: 'facebook-clone-8f0b2.appspot.com',
-  messagingSenderId: '571498687854',
-  appId: '1:571498687854:web:06df7bb6199d2c33ea257f',
-  measurementId: 'G-DYEMB23WDS',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
